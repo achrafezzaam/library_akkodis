1fd26ac0922461ee087deb6fea08b51519e34acd
Achraf Ezzaam <achraf.ezzaam@gmail.com>
2025-08-20 12:27:39 +0100

    feat: Implement POST /api/books endpoint
    Adds the route and controller required to add a new book document
    to the database.
    
    - Creates the `createBook` controller function.
    - Define the `POST /api/books` route in `bookRoutes.js`.
    - The endpoint returns a JSON of the newly created document

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -9,4 +9,23 @@ const getAllBooks = async (req, res) => {
     }
 };
 
-module.exports = { getAllBooks };
+const createBook = async (req, res) => {
+    try {
+        const {title, author, genre, publishedYear} = req.body;
+        const newBook = new Book({
+            title,
+            author,
+            genre,
+            publishedYear,
+        })
+        const savedBook = await newBook.save();
+        res.status(201).json(savedBook);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
+module.exports = {
+    getAllBooks,
+    createBook,
+};
--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const bookController = require('../controllers/bookController');
 
 router.route('/')
-    .get(bookController.getAllBooks);
-//     .post(bookController.createBook);
+    .get(bookController.getAllBooks)
+    .post(bookController.createBook);
 
 // router.route('/:id')
 //     .get(bookController.getBookById)
